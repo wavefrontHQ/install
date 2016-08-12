c0c883c7d0291375d12fbc714b802a6042ec6f11
Chung Kang Wang <chung@wavefront.com>
2016-08-12 01:18:40 +0000

    config file path fix for cassandra and redis plugin

--- WF-CDPInstaller/common/config.py
+++ WF-CDPInstaller/common/config.py
@@ -9,7 +9,7 @@ COLLECTD_HOME = '/etc/collectd'
 COLLECTD_CONF_DIR = COLLECTD_HOME + '/managed_config'
 PLUGIN_CONF_DIR = 'plugin_conf'
 PLUGIN_EXTENSION_DIR = 'plugin_extension'
-APP_DIR = '/tmp/app_configure'
+APP_DIR = '/tmp/WF-CDPInstaller'
 # Install state path and name
 INSTALL_STATE_FILE = 'install_state.json'
 INSTALL_STATE_FILE_PATH = '{}/{}'.format(
